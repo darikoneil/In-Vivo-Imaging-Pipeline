cc83078085b8f9ffe20e70c137a9af069bf7b64c
darikoneil <d.oneil@columbia.edu>
2022-12-09 16:17:40 -0500

    LATEST UPDATE - RAS - CONFIRMED

--- ImagingAnalysis/IO.py
+++ ImagingAnalysis/IO.py
@@ -14,34 +14,6 @@ import pathlib
 from prettytable import PrettyTable
 
 
-def _pretty_print_bruker_command(Channels, Planes, Frames, Height, Width) -> None:
-    """
-    Function simply prints the bruker folder contents detected
-
-    :param Channels: Number of Channels
-    :type Channels: int
-    :param Planes: Number of Planes
-    :type Planes: int
-    :param Frames: Number of Frames
-    :type Frames: int
-    :param Height: Height of Image (Y Pixels)
-    :type Height: int
-    :param Width:  Width of Image (X Pixels)
-    :type Width:
-    :rtype: None
-    """
-    _table = PrettyTable()
-    _table.header = False
-    _table.add_row(["Total Images Detected", Channels * Planes * Frames])
-    _table.add_row(["Channels", Channels])
-    _table.add_row(["Planes", Planes])
-    _table.add_row(["Frames", Frames])
-    _table.add_row(["Height", Height])
-    _table.add_row(["Width", Width])
-    print("\n")
-    print(_table)
-
-
 def determine_bruker_folder_contents(ImageDirectory: str) -> Tuple[int, int, int, int, int]:
     """
     Function determine contents of the bruker folder
@@ -198,6 +170,118 @@ def load_bruker_tiffs(ImageDirectory: str) -> Union[np.ndarray, Tuple[np.ndarray
         return images
 
 
+def load_single_tiff(Filename: str, NumFrames: int) -> np.ndarray:
+    """
+    Load a single tiff file
+
+    :param Filename: filename
+    :param NumFrames: number of frames
+    :type Filename: str
+    :type NumFrames: int
+    :return: numpy array [Z x Y x X]
+    :rtype: Any
+    """
+    return tifffile.imread(Filename, key=range(0, NumFrames, 1))
+
+
+def load_all_tiffs(ImageDirectory: str) -> np.ndarray:
+    """
+    Load a sequence of tiff stacks
+
+    :param ImageDirectory: Directory containing a sequence of tiff stacks
+    :type ImageDirectory: str
+    :return: complete_image numpy array [Z x Y x X] as int16
+    :rtype: Any
+    """
+    _fnames = os.listdir(ImageDirectory)
+    y_pix, x_pix = tifffile.TiffFile(ImageDirectory + "\\" + _fnames[0]).pages[0].shape
+    _num_frames = [] # initialize
+    [_num_frames.append(len(tifffile.TiffFile(ImageDirectory + "\\" + _fname).pages)) for _fname in _fnames]
+    _total_frames = sum(_num_frames)
+    complete_image = np.full((_total_frames, y_pix, x_pix), 0, dtype=np.int16)
+    _last_frame = 0
+
+    for _fname in tqdm(
+            range(len(_fnames)),
+            total=len(_fnames),
+            desc="Loading Images...",
+            disable=False,
+    ):
+        complete_image[_last_frame:_last_frame+_num_frames[_fname], :, :] = \
+            load_single_tiff(ImageDirectory + "\\" + _fnames[_fname], _num_frames[_fname])
+        _last_frame += _num_frames[_fname]
+
+    return complete_image
+
+
+def load_raw_binary(Filename: Union[str, None], MetaFile: Union[str, None], *args: Optional[str]) -> np.ndarray:
+    """
+    Loads a raw binary file
+
+    Enter the path to autofill (assumes Filename & meta are path + binary_video, video_meta.txt)
+
+    :param Filename: filename for binary video
+    :type Filename: str
+    :param MetaFile: filename for meta file
+    :type MetaFile: str
+    :param args: path to a directory containing Filename and MetaFile
+    :type args: str
+    :return: numpy array [Z x Y x X]
+    :rtype: Any
+    """
+    if len(args) == 1:
+        Filename = "".join([args[0], "\\binary_video"])
+        MetaFile = "".join([args[0], "\\video_meta.txt"])
+
+    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(MetaFile, delimiter=",", dtype="str")
+    _num_frames = int(_num_frames)
+    _x_pixels = int(_x_pixels)
+    _y_pixels = int(_y_pixels)
+    return np.reshape(np.fromfile(Filename, dtype=_type), (_num_frames, _y_pixels, _x_pixels))
+
+
+def load_binary_meta(Filename: str) -> Tuple[int, int, int, str]:
+    """
+    Loads meta file for binary video
+
+    :param Filename: The meta file (.txt ext)
+    :type Filename: str
+    :return: A tuple containing the number of frames, y pixels, and x pixels [Z x Y x X]
+    :rtype: tuple[int, int, int, str]
+    """
+    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(Filename, delimiter=",", dtype="str")
+    return int(_num_frames), int(_y_pixels), int(_x_pixels), str(_type)
+
+
+def load_mapped_binary(Filename: str, MetaFile: str, *args: Optional[str], **kwargs: str) -> np.memmap:
+    """
+    Loads a raw binary file in the workspace without loading into memory
+
+    Enter the path to autofill (assumes Filename & meta are path + binary_video, video_meta.txt)
+
+    :param Filename: filename for binary video
+    :type Filename: str
+    :param MetaFile: filename for meta file
+    :type MetaFile: str
+    :param args: Path
+    :type args: str
+    :keyword mode: pass mode to numpy.memmap (str, default = "r")
+    :return: memmap(numpy) array [Z x Y x X]
+    :rtype: Any
+    """
+    if len(args) == 1:
+        Filename = "".join([args[0], "\\binary_video"])
+        MetaFile = "".join([args[0], "\\video_meta.txt"])
+
+    _mode = kwargs.get("mode", "r")
+
+    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(MetaFile, delimiter=",", dtype="str")
+    _num_frames = int(_num_frames)
+    _x_pixels = int(_x_pixels)
+    _y_pixels = int(_y_pixels)
+    return np.memmap(Filename, dtype=_type, shape=(_num_frames, _y_pixels, _x_pixels), mode=_mode)
+
+
 def repackage_bruker_tiffs(ImageDirectory: str, OutputDirectory: str, *args: Union[int]) -> None:
     """
     Repackages a sequence of tiff files within a directory to a smaller sequence
@@ -317,118 +401,6 @@ def repackage_bruker_tiffs(ImageDirectory: str, OutputDirectory: str, *args: Uni
     return
 
 
-def load_single_tiff(Filename: str, NumFrames: int) -> np.ndarray:
-    """
-    Load a single tiff file
-
-    :param Filename: filename
-    :param NumFrames: number of frames
-    :type Filename: str
-    :type NumFrames: int
-    :return: numpy array [Z x Y x X]
-    :rtype: Any
-    """
-    return tifffile.imread(Filename, key=range(0, NumFrames, 1))
-
-
-def load_all_tiffs(ImageDirectory: str) -> np.ndarray:
-    """
-    Load a sequence of tiff stacks
-
-    :param ImageDirectory: Directory containing a sequence of tiff stacks
-    :type ImageDirectory: str
-    :return: complete_image numpy array [Z x Y x X] as int16
-    :rtype: Any
-    """
-    _fnames = os.listdir(ImageDirectory)
-    y_pix, x_pix = tifffile.TiffFile(ImageDirectory + "\\" + _fnames[0]).pages[0].shape
-    _num_frames = [] # initialize
-    [_num_frames.append(len(tifffile.TiffFile(ImageDirectory + "\\" + _fname).pages)) for _fname in _fnames]
-    _total_frames = sum(_num_frames)
-    complete_image = np.full((_total_frames, y_pix, x_pix), 0, dtype=np.int16)
-    _last_frame = 0
-
-    for _fname in tqdm(
-            range(len(_fnames)),
-            total=len(_fnames),
-            desc="Loading Images...",
-            disable=False,
-    ):
-        complete_image[_last_frame:_last_frame+_num_frames[_fname], :, :] = \
-            load_single_tiff(ImageDirectory + "\\" + _fnames[_fname], _num_frames[_fname])
-        _last_frame += _num_frames[_fname]
-
-    return complete_image
-
-
-def load_raw_binary(Filename: Union[str, None], MetaFile: Union[str, None], *args: Optional[str]) -> np.ndarray:
-    """
-    Loads a raw binary file
-
-    Enter the path to autofill (assumes Filename & meta are path + binary_video, video_meta.txt)
-
-    :param Filename: filename for binary video
-    :type Filename: str
-    :param MetaFile: filename for meta file
-    :type MetaFile: str
-    :param args: path to a directory containing Filename and MetaFile
-    :type args: str
-    :return: numpy array [Z x Y x X]
-    :rtype: Any
-    """
-    if len(args) == 1:
-        Filename = "".join([args[0], "\\binary_video"])
-        MetaFile = "".join([args[0], "\\video_meta.txt"])
-
-    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(MetaFile, delimiter=",", dtype="str")
-    _num_frames = int(_num_frames)
-    _x_pixels = int(_x_pixels)
-    _y_pixels = int(_y_pixels)
-    return np.reshape(np.fromfile(Filename, dtype=_type), (_num_frames, _y_pixels, _x_pixels))
-
-
-def load_binary_meta(Filename: str) -> Tuple[int, int, int, str]:
-    """
-    Loads meta file for binary video
-
-    :param Filename: The meta file (.txt ext)
-    :type Filename: str
-    :return: A tuple containing the number of frames, y pixels, and x pixels [Z x Y x X]
-    :rtype: tuple[int, int, int, str]
-    """
-    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(Filename, delimiter=",", dtype="str")
-    return int(_num_frames), int(_y_pixels), int(_x_pixels), str(_type)
-
-
-def load_mapped_binary(Filename: str, MetaFile: str, *args: Optional[str], **kwargs: str) -> np.memmap:
-    """
-    Loads a raw binary file in the workspace without loading into memory
-
-    Enter the path to autofill (assumes Filename & meta are path + binary_video, video_meta.txt)
-
-    :param Filename: filename for binary video
-    :type Filename: str
-    :param MetaFile: filename for meta file
-    :type MetaFile: str
-    :param args: Path
-    :type args: str
-    :keyword mode: pass mode to numpy.memmap (str, default = "r")
-    :return: memmap(numpy) array [Z x Y x X]
-    :rtype: Any
-    """
-    if len(args) == 1:
-        Filename = "".join([args[0], "\\binary_video"])
-        MetaFile = "".join([args[0], "\\video_meta.txt"])
-
-    _mode = kwargs.get("mode", "r")
-
-    _num_frames, _y_pixels, _x_pixels, _type = np.genfromtxt(MetaFile, delimiter=",", dtype="str")
-    _num_frames = int(_num_frames)
-    _x_pixels = int(_x_pixels)
-    _y_pixels = int(_y_pixels)
-    return np.memmap(Filename, dtype=_type, shape=(_num_frames, _y_pixels, _x_pixels), mode=_mode)
-
-
 def save_single_tiff(Images: np.ndarray, Filename: str, Type: Optional[np.dtype] = np.int16) -> None:
     """
     Save a numpy array to a single tiff file as type int16
@@ -510,3 +482,31 @@ def save_raw_binary(Images: np.ndarray, ImageDirectory: str) -> None:
 
     Images.tofile(_video_file)
     print("Finished saving images as a binary file.")
+
+
+def _pretty_print_bruker_command(Channels, Planes, Frames, Height, Width) -> None:
+    """
+    Function simply prints the bruker folder contents detected
+
+    :param Channels: Number of Channels
+    :type Channels: int
+    :param Planes: Number of Planes
+    :type Planes: int
+    :param Frames: Number of Frames
+    :type Frames: int
+    :param Height: Height of Image (Y Pixels)
+    :type Height: int
+    :param Width:  Width of Image (X Pixels)
+    :type Width:
+    :rtype: None
+    """
+    _table = PrettyTable()
+    _table.header = False
+    _table.add_row(["Total Images Detected", Channels * Planes * Frames])
+    _table.add_row(["Channels", Channels])
+    _table.add_row(["Planes", Planes])
+    _table.add_row(["Frames", Frames])
+    _table.add_row(["Height", Height])
+    _table.add_row(["Width", Width])
+    print("\n")
+    print(_table)
