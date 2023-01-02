2ce6d4899e31095a8897bd7a44e3e4a9e1670601
darikoneil <d.oneil@columbia.edu>
2023-01-01 19:13:20 -0500

    Documentation4

--- config_handler.py
+++ config_handler.py
@@ -26,3 +26,17 @@ def config_generator() -> None:
         print("Configuration File Generated.")
     _file.close()
 
+
+def config_reader() -> dict:
+    """
+    Reads config into a dictionary
+
+    :return:
+    """
+
+    _config_filename = "".join([getcwd(), "\\config.json"])
+
+    with open(_config_filename, "r") as _file:
+        _config = loads(_config_filename)
+
+    return _config
